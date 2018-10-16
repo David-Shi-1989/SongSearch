052677558dfd73154c2447fad94f33397b474605
David-Shi-1989 <287841605@qq.com>
2018-10-16 14:04:03 +0800

    添加vue框架，搭建开发环境(hot-middleware)

--- server/index.js
+++ server/index.js
@@ -3,35 +3,67 @@ const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const app = express()
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+
+const webpackServerConfig = require('../build/webpack.server.conf')
+const compiler = webpack(webpackServerConfig)
+
+const DIST_DIR = path.join(__dirname, '../', 'dist')
 
 var KuwoDriver = require('./plugins/kuwo')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('/static/*', function (req, res, next) {
+app.get('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
   var headerContentType = ''
   if (req.url.endsWith('js')) {
     headerContentType = 'application/javascript'
   } else if (req.url.endsWith('css')) {
     headerContentType = 'text/css'
+  } else {
+    headerContentType = 'application/json;charset=utf-8'
   }
-  var filePath = path.join(__dirname, '../', req.url)
-  fs.readFile(filePath, function (err, fr) {
-    if (err) {
+  res.header('Content-Type', headerContentType)
+  next()
+  // var filePath = path.join(__dirname, '../', req.url)
+  // fs.readFile(filePath, function (err, fr) {
+  //   if (err) {
 
-    } else {
-      res.writeHead(200, {'Content-Type':headerContentType})
-      res.end(fr)
+  //   } else {
+  //     res.writeHead(200, {'Content-Type': headerContentType})
+  //     res.end(fr)
+  //   }
+  // })
+})
+app.use('/static', express.static('static'))
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackServerConfig.output.publicPath,
+  noInfo: true
+}))
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get(['/', '/index.html'], (req, res, next) => {
+  const filename = path.join(DIST_DIR, 'index.html')
+  compiler.outputFileSystem.readFile(filename, function (err, result) {
+    if (err) {
+      return next(err)
     }
+    res.header('Content-Type', 'text/html; charset=UTF-8')
+    res.send(result)
+    res.end()
   })
 })
-
-app.get(['/','/index.html'], (req, res) => {
-  fs.readFile(path.join(__dirname, '../src/index.html'), function (err, fr) {
+app.get(['/bootstrap.html'], (req, res) => {
+  fs.readFile(path.join(__dirname, '../bootstrap/index.html'), function (err, fr) {
     if (err) {
-
     } else {
       res.header('Content-Type', 'text/html; charset=UTF-8')
       res.send(fr)
